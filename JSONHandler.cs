eaad40e4095c5cf758c2ea104f4bdf288ab5ccc0
WillSwift01 <103675097+WillSwift01@users.noreply.github.com>
2022-04-19 15:07:36 -0400

    Update JSONHandler.cs

--- JSONHandler.cs
+++ JSONHandler.cs
@@ -9,5 +9,22 @@ namespace PizzaProject
     //Not sure if this class is necessary or not, but we'll find out soon :)
     public class JSONHandler
     {
+        //Comment writing class outline idea
+        
+        //ALL WRONG but do not change
+        //Working on understanding how to write the handler
+        
+        //private string orderDB = "whatever the file path is";
+        //private string customerDB = "whatever the file path is";
+        //private string userDB = "whatever the file path is";
+        
+        //public void orderJSON(List<Order> orders, string type) {
+        // if (type == "read" || type == "Read") {
+        //    try/catch JsonConvert.SerializeObject(Order)
+        //   }
+        // if (type == "write" || type == "Write") {
+        //    try/catch JsonConvert.DeserializeObject(Order)
+        //   }
+        // }
     }
 }

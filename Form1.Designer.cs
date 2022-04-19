f3ad711accb7bd8b34278d962ca4ff41fc8568c6
uhhhAstrid <AstridWBusiness@gmail.com>
2022-04-19 12:57:09 -0400

    added some test GUI items for Luci

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace PizzaProject
+﻿using System;
+
+namespace PizzaProject
 {
     partial class Form1
     {
@@ -28,13 +30,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.anchovy = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // anchovy
+            // 
+            this.anchovy.AutoSize = true;
+            this.anchovy.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.anchovy.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.anchovy.Checked = true;
+            this.anchovy.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.anchovy.Location = new System.Drawing.Point(35, 40);
+            this.anchovy.Name = "anchovy";
+            this.anchovy.Size = new System.Drawing.Size(80, 17);
+            this.anchovy.TabIndex = 0;
+            this.anchovy.Text = "checkBox1";
+            this.anchovy.UseVisualStyleBackColor = true;
+            this.anchovy.CheckedChanged += new System.EventHandler(this.anchovy_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "now there\'s just text";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(35, 17);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.anchovy);
+            this.panel1.Location = new System.Drawing.Point(63, 56);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(254, 115);
+            this.panel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox anchovy;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace PizzaProject
         {
             InitializeComponent();
         }
+
+        private void anchovy_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }

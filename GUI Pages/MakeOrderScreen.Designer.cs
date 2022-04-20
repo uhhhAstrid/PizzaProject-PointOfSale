3c4cdf09068783668cdb26831c5155f6f71e3b0a
Luci Hill <lhill98@students.kennesaw.edu>
2022-04-20 15:58:51 -0400

    Edited Make Order Screen, Implemented ProcessPayment, Receipt, and Add New Customer Screens

--- GUI Pages/MakeOrderScreen.Designer.cs
+++ GUI Pages/MakeOrderScreen.Designer.cs
@@ -29,10 +29,11 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -47,15 +48,23 @@
             this.panel1.Size = new System.Drawing.Size(236, 305);
             this.panel1.TabIndex = 0;
             // 
-            // label1
+            // button2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(96, 10);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Add Item";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.button2.Location = new System.Drawing.Point(33, 199);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 56);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Drink";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 56);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Pizza";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
@@ -67,29 +76,29 @@
             this.label2.Text = "Add a  . . .";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // button1
+            // label1
             // 
-            this.button1.Location = new System.Drawing.Point(33, 112);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(170, 56);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Pizza";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Add Item";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // button2
+            // panel2
             // 
-            this.button2.Location = new System.Drawing.Point(33, 199);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(170, 56);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Drink";
-            this.button2.UseVisualStyleBackColor = true;
+            this.panel2.Location = new System.Drawing.Point(343, 35);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(430, 344);
+            this.panel2.TabIndex = 1;
             // 
             // MakeOrderScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "MakeOrderScreen";
             this.Text = "Form1";
@@ -106,5 +115,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
     }
 }

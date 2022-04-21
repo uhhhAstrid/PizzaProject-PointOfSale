3cb53cd340180d7e727be27c92771040086af734
WillSwift01 <williamswift01@yahoo.com>
2022-04-21 14:14:14 -0400

    bobba booey

--- JSONHandler.cs
+++ JSONHandler.cs
@@ -3,64 +3,87 @@ using System.Collections.Generic;
 using Newtonsoft.Json;
 using System.Data;
 using System.Diagnostics;
+using System.IO;
 
 namespace PizzaProject
 {
     //Not sure if this class is necessary or not, but we'll find out soon :)
     public class JSONHandler
     {
-        //Comment writing class outline idea
+
+        private string orderDBPath = "whatever the file path is";
+        private string customerDBPath = "whatever the file path is";
+        private string userDBPath = "whatever the file path is";
+
+        public void orderJSON(List<Order> orders, string type)
+        {
+            if (type == "read" || type == "Read")
+            {
+               JsonConvert.SerializeObject(orders);
+            }
+            if (type == "write" || type == "Write")
+            {
+                JsonConvert.DeserializeObject(Order);
+                return list of Orders;
+            }
+        }
+    
         
-        //ALL WRONG but do not change
-        //Working on understanding how to write the handler
-        
-        //private string orderDB = "whatever the file path is";
-        //private string customerDB = "whatever the file path is";
-        //private string userDB = "whatever the file path is";
-        
-        //public void orderJSON(List<Order> orders, string type) {
-        // if (type == "read" || type == "Read") {
-        //    try/catch JsonConvert.SerializeObject(orders);
-        //   }
-        // if (type == "write" || type == "Write") {
-        //    try/catch JsonConvert.DeserializeObject(Order);
-        //    return list of Orders;
-        //   }
-        // }
-        
-        //public void customerJSON(List<Customer> customers, string type) {
-        // if (type == "read" || type == "Read") {
-        //    try/catch JsonConvert.SerializeObject(cusotmers);
-        //   }
-        // if (type == "write" || type == "Write") {
-        //    try/catch JsonConvert.DeserializeObject(Customer);
-        //    return list of Customers;
-        //   }
-        // }
-        
-        //public void userJSON(List<User> users, string type) {
-        // if (type == "read" || type == "Read") {
-        //    try/catch JsonConvert.SerializeObject(users);
-        //   }
-        // if (type == "write" || type == "Write") {
-        //    try/catch JsonConvert.DeserializeObject(User);
-        //    return list of Users;
-        //   }
-        // }
-        
-        //public void checkJSONFile() {
-        // try { if (Application.datapath(orderDB) == null/false) {
-        //          Add Orders.json; }
-        //     }
-        // catch {}
-        // try { if (Application.datapath(customerDB) == null/false) {
-        //          Add Customers.json; }
-        //     }
-        // catch {}
-        // try { if (Application.datapath(userDB) == null/false) {
-        //          Add Users.json; }
-        //     }
-        // catch {}
-        // }
+
+        public void customerJSON(List<Customer> customers, string type)
+        {
+            if (type == "read" || type == "Read")
+            {
+                JsonConvert.SerializeObject(customers);
+            }
+            if (type == "write" || type == "Write")
+            {
+                JsonConvert.DeserializeObject();
+                return list of Customers;
+
+            }
+        }
+
+        public void userJSON(List<User> users, string type)
+        {
+            if (type == "read" || type == "Read")
+            {
+                JsonConvert.SerializeObject(users);
+            }
+            if (type == "write" || type == "Write")
+            {
+                JsonConvert.DeserializeObject(User);
+                return list of Users;
+
+            }
+        }
+
+        public void checkJSONFile()
+        {
+            try
+            {
+                if (File.Exists(orderDBPath) == false)
+                {
+                    File.Create(orderDBPath);
+                }
+            }
+            catch { }
+            try
+            {
+                if (File.Exists(customerDBPath) == false)
+                {
+                    File.Create(customerDBPath);
+                }
+            }
+            catch { }
+            try
+            {
+                if (File.Exists(userDBPath) == false)
+                {
+                    File.Create(userDBPath);
+                }
+            }
+            catch { }
+        }
     }
 }
